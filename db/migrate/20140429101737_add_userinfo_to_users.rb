35e03d137d3837fcd87c18ed61015ad91b63366d
Itagaki Kohta <ikaya3@gmail.com>
2014-04-29 19:27:00 +0900

    userid.username に制約を設ける
    unique 制約、 not-null 制約。

--- db/migrate/20140429101737_add_userinfo_to_users.rb
+++ db/migrate/20140429101737_add_userinfo_to_users.rb
@@ -1,6 +1,7 @@
 class AddUserinfoToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :userid, :string
-    add_column :users, :username, :string
+    add_column :users, :userid, :string, null: false, default: ""
+    add_column :users, :username, :string, null: false, default: ""
+    add_index :users, :userid, :unique => true
   end
 end
