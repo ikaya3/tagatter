9c847567b341e98ab019d2cbc2ee793728e3f512
Itagaki Kohta <ikaya3@gmail.com>
2014-04-29 19:27:18 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429094255) do
+ActiveRecord::Schema.define(version: 20140429101737) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -26,9 +26,12 @@ ActiveRecord::Schema.define(version: 20140429094255) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "userid",                 default: "", null: false
+    t.string   "username",               default: "", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["userid"], name: "index_users_on_userid", unique: true
 
 end
